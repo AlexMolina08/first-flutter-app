033ef57c34b55da5951009e4956bca024639ea61
alex <alex98criado@gmail.com>
2020-02-04 21:04:55 +0100

    Creación de los  métodos _buildRow(Que crea la fila) y _buildSuggestion (que crea un ListView)

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,48 @@ class PalabrasRandom extends StatefulWidget{
 //Devuelve una combinación de dos palabras
 class PalabrasRandomState extends State<PalabrasRandom> {
 
+  final _suggestions = <WordPair>[]; //Creamos una lista para almacenar las palabras creadas
+  final _fontSize = const TextStyle(fontSize: 18.0); //Tamaño de la fuente
+
+
+  //Devuelve el ListView
+  
+  Widget _buildSuggestions(){
+
+    return ListView.builder(
+
+      padding: const EdgeInsets.all(16.0),
+        //Función Anónima que se llama una vez por elemento en _suggestions
+        // Para cada fila , la funcion añade un ListTitle
+        // Para las filas impares , la funcion añade un ListDivider
+
+        //i: es un iterador que empieza en 0. Incrementa cada vez que se llama a la función
+        //context: BuildContext
+      itemBuilder: (context,i){
+
+        if(i.isOdd) return Divider();
+
+        final index = i ~/ 2;  //(División entera) Calcula el nº de elementos en el ListView menos
+                                //El widget Divider
+
+        if(_suggestions.length <= index) { //Si se ha llegado al final de los pares de palabras
+          _suggestions.addAll(generateWordPairs().take(10)); //Se generan 10 más y se añaden a la lista
+        }
+
+        return _buildRow(_suggestions[index]);
+
+      });
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _fontSize
+      ),
+    );
+  }
+
   Widget build(BuildContext context){
     final dosPalabras = WordPair.random();
     return Text(dosPalabras.asPascalCase);
